4c1a8551c6a839c743fced4d436e5be10b0f3404
Michal Valko <valko.miso111@gmail.com>
2024-12-21 12:17:15 +0100

    travel menu intro message placeholder

--- obj/Debug/net8.0/WorldOfZuul.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorldOfZuul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3208f730ada0ce38f783601a782bf241e8057903")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab20912e9f52b36f3a913e2cd2f9b9ab4893dcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
