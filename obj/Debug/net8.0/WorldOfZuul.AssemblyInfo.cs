26fbda9608e48e702ae3d90eadfd5a061fd9ea37
Michal Valko <valko.miso111@gmail.com>
2024-12-29 19:51:25 +0100

    removed 2 lines, game even easier to speedrun

--- obj/Debug/net8.0/WorldOfZuul.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorldOfZuul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4011268fafe14f42f3c19964b8fe4206404dc327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7270bf0f96005be403aa31d138f454b9e87619fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
